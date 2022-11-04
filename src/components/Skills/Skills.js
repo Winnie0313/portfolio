c94722234f5eccacad678cb065d5d16a0af0282b
Winnie0313 <liwanyi0313@gmail.com>
2022-11-03 18:31:28 -0700

    rename file

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { DiFirebase, DiReact, DiZend, DiJavascript, DiJavascript1 } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle, Icons } from './TechnologiesStyles';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle, Icons } from './SkillsStyles';
 
-const Technologies = () =>  (
+const Skills = () =>  (
   <Section id="tech">
     <SectionDivider />
     <SectionTitle>Skills</SectionTitle>
@@ -50,4 +50,4 @@ const Technologies = () =>  (
   </Section>
 );
 
-export default Technologies;
+export default Skills;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Acomplishments from '../components/Acomplishments/Acomplishments';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
+import Skills from '../components/Skills/Skills';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
@@ -15,7 +15,7 @@ const Home = () => {
         <BgAnimation />
       </Section>
       <Projects />
-      <Technologies />
+      <Skills />
       <Timeline />
       <Acomplishments />
     </Layout>
