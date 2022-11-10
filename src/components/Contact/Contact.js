b43af2a7e4e3d368da860c75a57e12fd9ea46d10
Winnie0313 <liwanyi0313@gmail.com>
2022-11-10 14:40:35 -0800

    add tel icon

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './ContactStyles';
-import { AiTwotoneMail } from 'react-icons/ai';
-
+import { AiTwotoneMail, AiFillPhone } from 'react-icons/ai';
 
 
 const Contact = () => (
@@ -14,7 +13,10 @@ const Contact = () => (
       <LinkColumn>
         <AiTwotoneMail size="4rem" />
         <LinkItem href='mailto:li.winnie0313@gmail.com'>li.winnie0313@gmail.com</LinkItem>
-
+      </LinkColumn>
+      <LinkColumn>
+        <AiFillPhone size="4rem" />
+        <LinkItem href='mailto:li.winnie0313@gmail.com'>li.winnie0313@gmail.com</LinkItem>
       </LinkColumn>
     </LinkList>
 
